70f5bdd73fe8519683b92834c61c0571fa8f2769
iuri aranda <iuri.aranda@gmail.com>
2016-11-17 12:15:56 +0000

    Create a kms_policy to grant usage of KMS keys to an IAM role

---
+++ kms_policy/main.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_policy" "kms_policy" {
+  name        = "kms-usage-policy"
+  description = "A policy to allow usage (encrypt & decrypt) of a KMS key"
+
+  policy = <<EOF
+{
+"Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "kms:Describe*",
+        "kms:Get*",
+        "kms:List*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt"
+      ],
+      "Effect": "Allow",
+      "Resource": "${var.kms_key_arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "kms_policy_attach" {
+  role       = "${var.aws_iam_role_name}"
+  policy_arn = "${aws_iam_policy.policy.arn}"
+}
---
+++ kms_policy/variables.tf
@@ -0,0 +1,7 @@
+variable "kms_key_arn" {
+  description = "ARN of the KMS key to use"
+}
+
+variable "aws_iam_role_name" {
+  default = "IAM role name to attach to the policy"
+}
