7a89cc59c4103977cf5aec52f34f8415a561cb8e
Luca Venturelli <venturel@users.noreply.github.com>
2016-12-19 16:17:12 +0100

    adding policy to access s3 bucket to install codedeploy agent and output role name (#5)
    * adding policy to access s3 bucket to install codedeploy agent and output kms role name
    
    * remove typo
    
    * fixing other typo
    
    * adding documentation
    
    * Update README.md

---
+++ codedeploy_packer_policy/main.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_policy" "codedeploy_install_policy" {
+  name        = "codedeploy-install-policy"
+  description = "A policy to allow download of codedeploy install script"
+
+  policy = <<EOF
+{
+"Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::aws-codedeploy-${var.region}/*"
+    }
+  ]
+}
+EOF
+}
