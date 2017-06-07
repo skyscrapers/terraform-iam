3d319e0ac4155b00f21939e709a49e6ab9af6e3b
Luca Venturelli <luca@skyscrapers.eu>
2017-06-07 23:35:48 +0200

    adding sns option

--- codedeploy_role/main.tf
+++ codedeploy_role/main.tf
@@ -1,5 +1,37 @@
 
+resource "aws_iam_role_policy" "codedeploy_policy_sns" {
+    count = "${var.sns_notify ? 1 : 0}"
+    name = "codedeploy"
+    role = "${aws_iam_role.codedeploy_role.id}"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:CompleteLifecycleAction",
+                "autoscaling:DeleteLifecycleHook",
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeLifecycleHooks",
+                "autoscaling:PutLifecycleHook",
+                "autoscaling:RecordLifecycleActionHeartbeat",
+                "ec2:DescribeInstances",
+                "ec2:DescribeInstanceStatus",
+                "tag:GetTags",
+                "tag:GetResources",
+                "sns:Publish"
+
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_role_policy" "codedeploy_policy" {
+    count = "${var.sns_notify ? 0 : 1}"
     name = "codedeploy"
     role = "${aws_iam_role.codedeploy_role.id}"
     policy = <<EOF
@@ -19,6 +51,7 @@ resource "aws_iam_role_policy" "codedeploy_policy" {
                 "ec2:DescribeInstanceStatus",
                 "tag:GetTags",
                 "tag:GetResources"
+
             ],
             "Resource": "*"
         }
--- codedeploy_role/variables.tf
+++ codedeploy_role/variables.tf
@@ -2,3 +2,6 @@ variable "region" {
   description = "Region where codedeploy will run in"
   default = "eu-west-1"
 }
+variable "sns_notify" {
+  default = false
+}
