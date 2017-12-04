d2275a4d09c8a6f7ac29a8f219e810c8282aa56c
Mattias Gees <mattias.gees@gmail.com>
2017-12-04 11:15:35 +0300

    Add description to outputs

--- packer_policy/outputs.tf
+++ packer_policy/outputs.tf
@@ -1,11 +1,14 @@
 output "packer_policy_arn" {
-  value = "${aws_iam_policy.packer_policy.arn}"
+  description = "The generated policy ARN"
+  value       = "${aws_iam_policy.packer_policy.arn}"
 }
 
 output "packer_policy_name" {
-  value = "${aws_iam_policy.packer_policy.name}"
+  description = "The generated policy name"
+  value       = "${aws_iam_policy.packer_policy.name}"
 }
 
 output "packer_policy_id" {
-  value = "${aws_iam_policy.packer_policy.id}"
+  description = "The generated policy id"
+  value       = "${aws_iam_policy.packer_policy.id}"
 }
