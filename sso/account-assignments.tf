94adac49d3c29de3b0fdebe4efbab0d5ae8310fd
Simon Rondelez <simonrondelez@me.com>
2025-07-07 16:14:27 +0200

    chore: Replace deprecated filter skyscrapers/platform#1496 (#27)

--- sso/account-assignments.tf
+++ sso/account-assignments.tf
@@ -20,9 +20,11 @@ data "aws_identitystore_group" "groups" {
 
   identity_store_id = var.sso_instance_id
 
-  filter {
-    attribute_path  = "DisplayName"
-    attribute_value = each.value
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = each.value
+    }
   }
 }
 
